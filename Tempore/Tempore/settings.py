f7dbe8460b52da1620c41886683e4304f3e9c3eb
Unknown <scott.alm@elev.ga.klaragymnasium.se>
2019-01-10 10:56:01 +0100

    a commit

--- Tempore/Tempore/settings.py
+++ Tempore/Tempore/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+MONGODB_URI = os.environ.get('MONGODB_URI', "mongodb://scotter:bas@ds119024.mlab.com:19024/tempore_test_base")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
