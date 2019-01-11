f66bb048653e8f11a7268c1336d0e8a1ea2be348
datorsalen <scottalm83@gmail.com>
2019-01-11 11:25:44 +0100

    S commit

--- Tempore/TemporeApp/views.py
+++ Tempore/TemporeApp/views.py
@@ -1,8 +1,10 @@
 import logging
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login, authenticate
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .services.response_service import main_response_handler
+from .core.forms import SignUpForm
 
 log = logging.getLogger(__file__)
 
@@ -17,6 +19,20 @@ def get_person_coordinates_from_location(request):
         log.warning("Failed to get location", exc_info=True)
     return Response(status=404)
 
-
 def render_website(request):
     return render(request, '/index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('passqord1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            return redirect('index.html')
+        else:
+            form = SignUpForm
+            return render(request, 'signup.html', {'form': form})
