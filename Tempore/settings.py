320c4a6343fa23c06d444183b618bbf3c217863e
scott alm <scott.alm@elev.ga.klaragymnasium.se>
2019-01-25 15:14:42 +0100

    ProcFile update

--- Tempore/settings.py
+++ Tempore/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -47,7 +48,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'accounts.apps.AccountsConfig',
     'TemporeApp',
-    'corsheaders'
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -87,13 +88,19 @@ WSGI_APPLICATION = 'Tempore.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {'default':
+                 dj_database_url.config(default='postgres://cganjobgdtcrtp:283d66529e2c303896a229438f14dd250edcd809854753330d84a79cdabaaae4@ec2-46-137-170-51.eu-west-1.compute.amazonaws.com:5432/d47vhjqemmufb3')}
+if ENV == 'DEBUG':
+    DATABASES['default'] = \
+        {'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+         }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
