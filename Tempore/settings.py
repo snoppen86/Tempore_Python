0b677634f558bf4fef16f3f5c6822eef35c6b987
scott alm <scott.alm@elev.ga.klaragymnasium.se>
2019-01-25 15:47:49 +0100

    StaticfilE update

--- Tempore/settings.py
+++ Tempore/settings.py
@@ -52,8 +52,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
