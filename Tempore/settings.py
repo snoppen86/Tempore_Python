399a504c261d46014f491179f43b3eb9a32f3680
scott alm <scott.alm@elev.ga.klaragymnasium.se>
2019-01-25 15:37:29 +0100

    StaticfilE update

--- Tempore/settings.py
+++ Tempore/settings.py
@@ -145,9 +145,7 @@ CRISPY_TEMPLATES_PACK = 'bootstrap4'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGGING = {
